158d21dc0803d3dad0cb0fb7ce9d3d2741e17ada
aashish-krsahu <krsahu248@gmail.com>
2025-07-24 16:17:11 +0530

    Classes

--- optimal task allocation.cpp
+++ optimal task allocation.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class student{
+    int id;
+    string name;
+    int reliability;
+};
+
+class task{
+    int id;
+    string name;
+    int difficulty;
+};
+
+class edge{
+    int student_id;
+    int cost;
+    int task_id;
+};
+
 int main(){
     
 
