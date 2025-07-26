1a5f059b8a37f537968e582e944c641d80370450
aashish-krsahu <krsahu248@gmail.com>
2025-07-26 10:34:14 +0530

    final cost calculation

--- optimal task allocation.cpp
+++ optimal task allocation.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <math.h>
 using namespace std;
 
 class student_class{
@@ -8,6 +9,9 @@ class student_class{
     string name;
     int reliability;
 public:
+    int get_reliability() {
+        return reliability;
+    }
     vector<student_class> read_students();
 
 };
@@ -17,6 +21,9 @@ class task_class{
     string name;
     int difficulty;
 public:
+    int get_difficulty(){
+        return difficulty;
+    } 
     vector<task_class> read_tasks();
 };
 
@@ -24,6 +31,8 @@ class edge_class{
     int student_id;
     int cost;
     int task_id;
+public:
+    vector<vector<float>> read_edges(float , float );
 };
 
 vector<student_class> student_class:: read_students(){
@@ -35,9 +44,9 @@ vector<student_class> student_class:: read_students(){
         getline(in,id,',');
         getline(in,name,',');
         getline(in,reliability,'\n');
-        int id = stoi(id);
+        int sid = stoi(id);
         int reliability = stoi(reliability);
-        students.emplace_back(id, name, reliability);
+        students.emplace_back(sid, name, reliability);
     }
     in.close();
     return students;
@@ -52,13 +61,36 @@ vector<task_class> task_class :: read_tasks(){
         getline(in,id,',');
         getline(in,name,',');
         getline(in,difficulty,'\n');
-        int id = stoi(id);
+        int tid = stoi(id);
         int difficulty = stoi(difficulty);
-        task.emplace_back(id,name,difficulty);
+        task.emplace_back(tid,name,difficulty);
     }
     in.close();
     return task;
 }
+
+vector<vector<float>> edge_class:: read_edges(float alpha, float beta){
+    student_class obj1;
+    task_class obj2;
+    vector<student_class> students = obj1.read_students();
+    vector<task_class> tasks = obj2.read_tasks();
+    vector<vector<float>> final_cost(students.size(), vector<float>(tasks.size(),1e6));
+    
+    ifstream in("edges.csv");
+    while(!in.eof()){
+        string ssid,stid,scost;
+        getline(in,ssid,',');
+        getline(in,stid,',');
+        getline(in, scost, '\n');
+        float sid = stof(ssid) - 1.0;
+        float tid = stof(stid) - 1.0;
+        float cost = stof(scost);
+        final_cost[sid][tid] = beta * cost + alpha * abs(students[sid].get_reliability() - tasks[tid].get_difficulty());
+    }
+    return final_cost;
+}
+
+
 
 int main(){
     float weightage_reliability = 0;
